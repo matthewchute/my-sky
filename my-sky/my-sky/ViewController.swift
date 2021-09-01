f36dacc19f5df5840f15ac220f753443c4e88286
matthewchute <mchute@sfu.ca>
2021-09-01 16:31:59 -0700

    init project

---
+++ my-sky/my-sky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-sky
+//
+//  Created by Matthew Chute on 2021-09-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
